97519c43cd482a9ec4009cc5e48abe90ff024167
Iditye <idit_y@radiflow.com>
2022-02-24 11:57:34 +0200

    Updating the name of the button

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import { Button } from "./Styles/Button.styled"
+import { StyledButton } from "./Styles/Button.styled"
 import { Container } from "./Styles/Container.styled"
 import { Flex } from "./Styles/Flex.styled"
 import { StyledHeader, Nav, Logo, Image } from "./Styles/Header.styled"
@@ -9,13 +9,13 @@ export default function Header() {
             <Container>
                 <Nav>
                     <Logo src='./images/logo.svg' alt='something'/>
-                    <Button>Try it free</Button>
+                    <StyledButton>Try it free</StyledButton>
                 </Nav>
                 <Flex>
                     <div>
                         <h1>Build the Community Your Fans will Love</h1>
                         <p>Sunt reprehenderit anim exercitation commodo ex. Consequat laborum minim proident anim reprehenderit duis nostrud ea consectetur est. Adipisicing id aute dolore laboris cillum. Nisi esse pariatur occaecat commodo sint eiusmod nulla amet irure officia elit sit. Est eu magna ea adipisicing consectetur amet sunt laborum.</p>
-                        <Button bg="#ff0099" color="#fff">Get Started for Free</Button>
+                        <StyledButton bg="#ff0099" color="#fff">Get Started for Free</StyledButton>
                     </div>
                     <Image src="./images/illustration-mockups.svg" alt=""/>
                 </Flex>
